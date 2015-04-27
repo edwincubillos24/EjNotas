f9af8b24657cf85f410f4e6875ea9212e59f5fdc
Edwin <edwin.cubillos@udea.edu.co>
2015-04-27 16:03:18 -0500

    Version3.0: Se crea nueva actividad "AboutActivity" Se crea un menú para abrir la actividad por medio de un Intent Se modifica el tema de la nueva actividad para que quede como un cuadro de dialogo Dos Idiomas: Ingles, Español

--- app/src/main/java/com/example/ejnotas/MainActivity.java
+++ app/src/main/java/com/example/ejnotas/MainActivity.java
@@ -8,7 +8,6 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -38,6 +37,23 @@ public class MainActivity extends ActionBarActivity {
                 NotaF.setText(String.valueOf(nota));
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.aboutUs){
+            Intent i = new Intent(this, AboutActivity.class);
+            startActivity(i);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
 
